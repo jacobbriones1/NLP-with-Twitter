4d9b91abe238bbd8697df2285fa2984f48124c9b
Jacob Briones <70331998+jacobbriones1@users.noreply.github.com>
2020-09-13 21:52:46 -0700

    update comments

--- getTweets.py
+++ getTweets.py
@@ -4,42 +4,46 @@ import tweepy
 import datetime
 import os
 import json
-
-#  Import credentials
+#  For authenticating Twitter API
 import credentials
-
+# For streaming live tweets
 from tweepy import Stream
 
-# Set up words to track  
-keywords_to_track = ['#trump','#biden','donald trump','donaldtrump','biden','joebiden', 'joe biden']
-#  Retrieve credentials
-consumer_key = credentials.CONSUMER_KEY
-consumer_secret = credentials.CONSUMER_SECRET
-access_token = credentials.ACCESS_TOKEN
-access_secret = credentials.ACCESS_SECRET
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
-
-
+def getTweets(ketwords_to_track):
+        #  Retrieve credentials
+        consumer_key = credentials.CONSUMER_KEY
+        consumer_secret = credentials.CONSUMER_SECRET
+        access_token = credentials.ACCESS_TOKEN
+        access_secret = credentials.ACCESS_SECRET
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
         
-api = tweepy.API(auth,wait_on_rate_limit=True,)
-tweets = []
-# file name that you want to open is the second argument
-save_file = open('tweets.json', 'a')
+        #  Authorization 
+        auth.set_access_token(access_token, access_secret)
+        
+        # Instantiate Twitter API.
+        # NOTE:Twitter limits the amount of data that a user may stream.
+        #   if a user revcieves a rate_limit warning, then the user must 
+        #   wait one hour before accessing data. The wait_on_rate_limit
+        #   parameter ensures that our program will not terminate if this
+        #   error occurs.
+        api = tweepy.API(auth, wait_on_rate_limit=True,)
+        tweets = []
+        # file name that you want to open is the second argument
+        save_file = open('tweets.json', 'a')
 
-class CustomStreamListener(tweepy.StreamListener):
-    def __init__(self, api):
-        self.api = api
-        super(tweepy.StreamListener, self).__init__()
-        self.save_file = tweets
+        class CustomStreamListener(tweepy.StreamListener):
+            def __init__(self, api):
+                self.api = api
+                super(tweepy.StreamListener, self).__init__()
+                self.save_file = tweets
 
-    def on_data(self, tweet):
-        self.save_file.append(json.loads(tweet))
-        print(tweet)
-        save_file.write(str(tweet))
+            def on_data(self, tweet):
+                self.save_file.append(json.loads(tweet))
+                print(tweet)
+                save_file.write(str(tweet))
 
-stream = Stream(auth, CustomStreamListener(api))
-stream.filter(track=keywords_to_track)
+        stream = Stream(auth, CustomStreamListener(api))
+        stream.filter(track=keywords_to_track)
 
 
 
