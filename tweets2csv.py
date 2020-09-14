ec37f9344d1bdb92727fff231f6c1470855e955b
Jacob Briones <70331998+jacobbriones1@users.noreply.github.com>
2020-09-13 23:32:59 -0700

    comments

--- tweets2csv.py
+++ tweets2csv.py
@@ -1,37 +1,53 @@
+#  The tweets2csv.py program takes the tweets.json file created by the getTweets.py function
+#   and returns a csv named tweets.csv containing the relevant information which we will
+#   be processed.
+#   Written by Jacob Briones
+
 import json
 import pandas as pd
 import numpy as np
+
+#  Set file path
 tweets_data_path = "tweets.json"
 tweets_data = []  
+
+#  open and read in file
 tweets_file = open(tweets_data_path, "r")  
 for line in tweets_file:  
+    # Here, the user can specify what language we want our data to have.
     try:  
         tweet = json.loads(line)
         if tweet['lang']=='en':
             tweets_data.append(tweet)  
     except:  
         continue
+
+# Create Pandas Data Frame and specify columns.
 tweets = pd.DataFrame()
 tweets['Username'] = list(map(lambda tweet: tweet['user']['screen_name'], tweets_data))
 tweets['Text'] = list(map(lambda tweet: tweet['text'], tweets_data))
+tweets['Followers'] = list(map(lambda tweet: tweet['user']['followers_count'], tweets_data))
+tweets['Friends'] = list(map(lambda tweet: tweet['user']['friends_count'], tweets_data))
+
+# Hashtags are contained in an 'entities' object. This file may be empty,
+#   so we must specify what we want to store if a tweet has no hashtags.
 hashtags = []
 for tweet in tweets_data:
     if len(tweet['entities']['hashtags'])>0:
         j = len(tweet['entities']['hashtags'])
         toappend = []
+        
         for i in range(j):
             toappend.append(tweet['entities']['hashtags'][i]['text'])
         hashtags.append(toappend)
     else:
         hashtags.append('')
 
-    
 tweets['Hashtags'] = hashtags
-tweets['Followers'] = list(map(lambda tweet: tweet['user']['followers_count'], tweets_data))
-tweets['Friends'] = list(map(lambda tweet: tweet['user']['friends_count'], tweets_data))
 
-
-retweetedUsers = []
+#  If the tweet is a retweet, then the text will contain '@ RT'. 
+#   This string usually does not provide any meaningful information
+#   for us, so I chose to remove it. 
 for i in range(len(tweets.Text)):
     if tweets.Text[i].startswith('RT @') == True:
         try:
@@ -39,5 +55,6 @@ for i in range(len(tweets.Text)):
             tweets.Text=tweets.Text.replace(tweets.Text[i],retweetText)
         except:
             continue
-        
-tweets.to_csv('twitter_trump_biden.csv', index = False)
+
+# Save the pandas dataframe to a csv file.
+tweets.to_csv('tweets.csv', index = False)
